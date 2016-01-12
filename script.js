9004993d5f420fa9a84dd7b1a0a0e5ffbdebad93
Andrew Overby <andrewdoverby@gmail.com>
2016-01-12 17:41:58 -0600

    waypoints and add jquery

--- script.js
+++ script.js
@@ -8,3 +8,12 @@
                                     
 //  }); 
 // above is an exercise in understanding only
+
+
+
+/*  var waypoints = $('#handler-first').waypoint(function(direction) {
+  notify(this.element.id + ' hit 25% from top of window') 
+}, {
+  offset: '25%'
+})    
+*/
