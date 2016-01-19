7057423de354ac7f60794bfba67056d8206f1de0
Andrew Overby <andrewdoverby@gmail.com>
2016-01-19 00:40:29 -0600

    done animating cs

--- script.js
+++ script.js
@@ -41,6 +41,26 @@
     /* Animations on scroll */ 
      $('.js--wp-1').waypoint(function(direction) {
         $('.js--wp-1').addClass('animated fadeIn');  
-     })
+     }, {
+         offset: '500'
+     });
+      $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');  
+     }, {
+         offset: '500'
+     });
+     
+      $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');  
+     }, {
+         offset: '500'
+     });
+     
+      $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');  
+     }, {
+         offset: '500'
+     });
+     
      
 });
