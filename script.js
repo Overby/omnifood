95a1e998d444d27a44a52a26b633a78f1f14c8ed
Andrew Overby <andrewdoverby@gmail.com>
2016-01-19 20:29:08 -0600

    tried to compress images

--- script.js
+++ script.js
@@ -64,7 +64,18 @@
      
 /* Mobile nav */
      $('.js--nav-icon').click(function() {
-       var nav = $('.js--main-nav')  
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+         
+        nav.slideToggle(200); /* milliseconds==2sec */
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round')
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+                  
      });
      
      
